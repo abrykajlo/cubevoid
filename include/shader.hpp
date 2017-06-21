76f8f8324428633522fc52deb57fa365f969b1d0
Adam Brykajlo <adam.brykajlo@gmail.com>
2017-06-21 07:07:17 -0700

    work on shaders and shader compilation

--- include/shader.hpp
+++ include/shader.hpp
@@ -3,18 +3,29 @@
 #include <GL/glew.h>
 #include <util.hpp>
 
+enum ShaderType
+{
+	VERTEX_SHADER = GL_VERTEX_SHADER,
+	TESS_CONTROL_SHADER = GL_TESS_CONTROL_SHADER,
+	TESS_EVALUATION_SHADER = GL_TESS_EVALUATION_SHADER,
+	GEOMETRY_SHADER = GL_GEOMETRY_SHADER,
+	FRAGMENT_SHADER = GL_FRAGMENT_SHADER,
+	COMPUTE_SHADER = GL_COMPUTE_SHADER
+};
+
 class Shader
 {
 public:
 	friend class ShaderProgram;
 
-	Shader();
+	Shader(ShaderType st);
 	~Shader();
 
 	int SetSource(const char*);
 	int Compile();
 	const char* GetError();
 private:
+	ShaderType shaderType_;
 	const char* source_;
 	GLuint shaderId_;
 };
--- src/shader.cpp
+++ src/shader.cpp
@@ -1,7 +1,11 @@
 #include "shader.hpp"
 
-Shader::Shader() : source_(nullptr)
-{}
+Shader::Shader(ShaderType st) 
+	: source_(nullptr),
+	  shaderType_(st)
+{
+	shaderId_ = glCreateShader(st);
+}
 
 Shader::~Shader()
 {
@@ -15,17 +19,13 @@ Shader::~Shader()
 
 int Shader::SetSource(const char* source)
 {
-	if (source_ != nullptr)
-	{
-		delete source_;
-	}
-
-	source_ = source;
+	glShaderSource(shaderId_, 1, &source, nullptr);
 	return 0;
 }
 
 int Shader::Compile()
 {
+	glCompileShader(shaderId_);
 	return 0;
 }
 
@@ -34,12 +34,38 @@ const char * Shader::GetError()
 	return nullptr;
 }
 
+ShaderProgram::ShaderProgram()
+	: head_(nullptr),
+	  tail_(nullptr)
+{}
+
 ShaderProgram::~ShaderProgram()
 {
+	//TODO: need to free up ShaderNodes
 }
 
-int ShaderProgram::AddShader(Shader & shader)
+int ShaderProgram::AddShader(Shader& shader)
 {
+	if (head_ == nullptr)
+	{
+		head_ = new ShaderNode(&shader);
+		tail_ = head_;
+	}
+	else
+	{
+		switch (tail_->data->shaderType_)
+		{
+		case VERTEX_SHADER:
+		case TESS_CONTROL_SHADER:
+		case TESS_EVALUATION_SHADER:
+		case GEOMETRY_SHADER:
+		case FRAGMENT_SHADER:
+		}
+		auto temp = new ShaderNode(&shader);
+		tail_->next = temp;
+		tail_ = temp;
+	}
+
 	return 0;
 }
 
@@ -48,7 +74,7 @@ int ShaderProgram::Compile()
 	return 0;
 }
 
-const char * ShaderProgram::GetError()
+const char* ShaderProgram::GetError()
 {
 	return nullptr;
 }
