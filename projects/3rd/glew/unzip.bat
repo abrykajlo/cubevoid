03d81972d0e67406ffff8cb25b4f9f206d62af82
Adam Brykajlo <adam.brykajlo@gmail.com>
2017-07-02 12:49:23 -0700

    project directory rewrite part 2

--- projects/3rd/glew/unzip.bat
+++ projects/3rd/glew/unzip.bat
@@ -1,12 +1,11 @@
-echo 7z x -y .\glew-2.0.0.zip
 7z x -y .\glew-2.0.0.zip
 
-if not exist "..\..\..\include\GL\" (
-	mkdir ..\..\..\include\GL
+if not exist ".\GL\" (
+	mkdir .\GL
 )
-copy .\glew-2.0.0\include\GL\glew.h ..\..\..\include\GL\
-copy .\glew-2.0.0\include\GL\wglew.h ..\..\..\include\GL\
+copy .\glew-2.0.0\include\GL\glew.h .\GL\
+copy .\glew-2.0.0\include\GL\wglew.h .\GL\
 
-copy .\glew-2.0.0\src\glew.c ..\..\..\src\glew.c
+copy .\glew-2.0.0\src\glew.c .\glew.c
 
 type nul > .\timestamp.txt
--- projects/3rd/lua/unzip.bat
+++ projects/3rd/lua/unzip.bat
@@ -1,15 +1,11 @@
 7z x -y .\lua-5.3.4.tar.gz
 7z x -y .\lua-5.3.4.tar
 
-if not exist "..\..\..\include\lua\" (
-	mkdir ..\..\..\include\lua
+if not exist ".\lua\" (
+	mkdir .\lua
 )
-copy .\lua-5.3.4\src\*.h ..\..\..\include\lua
-copy .\lua-5.3.4\src\lua.hpp ..\..\..\include\lua
-
-if not exist "..\..\..\src\lua\" (
-	mkdir ..\..\..\src\lua
-)
-copy .\lua-5.3.4\src\*.c ..\..\..\src\lua
+copy .\lua-5.3.4\src\*.h .\lua
+copy .\lua-5.3.4\src\lua.hpp .\lua
+copy .\lua-5.3.4\src\*.c .\lua
 
 type nul > .\timestamp.txt
